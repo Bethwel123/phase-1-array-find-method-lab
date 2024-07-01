71d4c5c727c07fc85fc688cc69bb9e322b90f2c5
Bethwel123 <bethwellangat654@gmail.com>
2024-07-01 16:22:54 +0300

    Using array find() in functions

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+// Write your code here
+function superbowlWin(results) {
+    const win = results.find(function(game){
+        return game.result === "W";
+    });
+    
+    if(win) {
+        return win.year;
+    }else {
+        return undefined;
+    }
+}
+
